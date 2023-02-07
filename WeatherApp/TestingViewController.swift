e45437110789ad3b72cff373bc6e47de560dc8c2
silthice <ryonlim@hotmail.com>
2023-02-07 14:52:03 +0800

    - Added test case

---
+++ WeatherApp/TestingViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TestingViewController.swift
+//  WeatherApp
+//
+//  Created by Giap on 07/02/2023.
+//
+
+import UIKit
+
+class TestingViewController: UIViewController {
+    override func viewDidLoad() {
+        let stringNums = [1,2,3].map({ String($0)})
+    }
+}
+
+func test1(age1:Int) {
+    
+}
+
+func test2(age1: Int) {
+    
+}
+
+func test3(age3_:Int) {
+    
+}
+func test4(age4_: Int) {
+    
+}
+
+func test5(age5: Int){
+}
+
+func test6(age6: Int)
+{
+}
